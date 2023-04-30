c07e2da641f8ddfdba2b782fa15c3a58b02cee4e
JeyranHumbatova <127186057+JeyranHumbatova@users.noreply.github.com>
2023-04-30 10:58:32 +0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const inp=document.querySelectorAll("input")[0];
+const inp2=document.querySelectorAll("input")[1];
+const btn=document.querySelector("button");
+let write=[];
+
+function addArr() {
+    write.push(inp.value);
+    document.getElementById("text").style.color= inp2.value;
+    document.getElementById("text").innerHTML = write;
+}
